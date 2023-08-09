9eaaaa9b321b3e1bf2ca7a18883ee2e20f36d980
Maria Alejandra <m.alejamonsalve@hotmail.com>
2023-08-08 21:02:51 -0500

    agregue servidor lista de tareas

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 const readline = require('readline');
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+const app = express();
+const server = http.createServer(app);
+
+app.use(bodyParser.json());
+
 const tasks = [];
 
-function addTask(indicador, descripcion) {
+let taskIdCounter = 1;
+
+function addTask(description) {
   return new Promise((resolve, reject) => {
-    tasks.push({ indicador, descripcion, completada: false });
-    resolve();
+    const task = {
+      id: taskIdCounter,
+      description: description,
+      completed: false
+    };
+    tasks.push(task);
+    taskIdCounter++;
+    resolve(task);
   });
 }
 
-function removeTask(indicador) {
+function removeTask(id) {
   return new Promise((resolve, reject) => {
-    const index = tasks.findIndex(task => task.indicador === indicador);
+    const index = tasks.findIndex(task => task.id === id);
     if (index !== -1) {
       tasks.splice(index, 1);
     }
@@ -24,43 +40,59 @@ function removeTask(indicador) {
   });
 }
 
-function completeTask(indicador) {
+function completeTask(id) {
   return new Promise((resolve, reject) => {
-    const task = tasks.find(task => task.indicador === indicador);
+    const task = tasks.find(task => task.id === id);
     if (task) {
-      task.completada = true;
+      task.completed = true;
     }
     resolve();
   });
 }
 
-function showTasks() {
-  console.log('\nLista de tareas:');
-  tasks.forEach(task => {
-    console.log(`${task.indicador}: ${task.descripcion} - ${task.completada ? 'Completada' : 'Pendiente'}`);
-  });
-}
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+});
+
+app.post('/tasks', async (req, res) => {
+  const { description } = req.body;
+  const newTask = await addTask(description);
+  res.json(newTask);
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  await removeTask(id);
+  res.json({ message: 'Task removed successfully' });
+});
+
+app.put('/tasks/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  await completeTask(id);
+  res.json({ message: 'Task completed successfully' });
+});
 
 function promptAction() {
-  rl.question('\nElige una acción (add/remove/complete/show/exit): ', async action => {
+  rl.question('\nElige una acción (add/remove/complete/show/exit): ', action => {
     switch (action) {
       case 'add':
-        rl.question('Nombre de la tarea: ', async indicador => {
-          rl.question('Descripción de la tarea: ', async descripcion => {
-            await addTask(indicador, descripcion);
-            promptAction();
-          });
+        rl.question('Descripción de la tarea: ', async description => {
+          const newTask = await addTask(description);
+          console.log('Tarea añadida:', newTask);
+          promptAction();
         });
         break;
       case 'remove':
-        rl.question('Cual tarea quieres eliminar?: ', async indicador => {
-          await removeTask(indicador);
+        rl.question('ID de la tarea que quieres eliminar?: ', async id => {
+          await removeTask(parseInt(id));
+          console.log('Tarea eliminada');
           promptAction();
         });
         break;
       case 'complete':
-        rl.question('Cual fue la tarea que realizaste?: ', async indicador => {
-          await completeTask(indicador);
+        rl.question('ID de la tarea que has completado?: ', async id => {
+          await completeTask(parseInt(id));
+          console.log('Tarea completada');
           promptAction();
         });
         break;
@@ -79,5 +111,19 @@ function promptAction() {
   });
 }
 
+function showTasks() {
+  console.log('\nLista de tareas:');
+  tasks.forEach(task => {
+    console.log(`${task.id}: ${task.description} - ${task.completed ? 'Completada' : 'Pendiente'}`);
+  });
+}
+
 console.log('Bienvenido a la lista de tareas!');
 promptAction();
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
+});
+
